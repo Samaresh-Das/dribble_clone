51764d36707383a93bcfaf8ed87c4df526135d03
Samaresh Das <samareshmail679@gmail.com>
2023-07-26 21:46:44 +0530

    tried to fix the category undefine dbug

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { type } from "os";
 import React from "react";
 
 type SearchParams = {
-  category?: string | undefined;
+  category?: string | "";
   endcursor: string;
 };
 type ProjectSearch = {
